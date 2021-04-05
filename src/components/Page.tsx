aab985018986e22e8c92e4c7aa2fd03f207da30d
Sergey Zabelin <ns6630@gmail.com>
2021-04-06 00:00:38 +0300

    Добавлена статика. Создан компонент Page.

---
+++ src/components/Page.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import bg from "../static/img/bg.png";
+
+const Page = styled.div`
+  margin: 0;
+  padding: 0;
+  background-image: url(${bg});
+  background-repeat: repeat;
+  min-height: 100vh;
+  font-family: 'Open Sans', sans-serif;
+  color: #fbf7f9;
+`;
+
+export default Page;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './static/css/reset.css';
+import './static/css/fonts.css';
 
 ReactDOM.render(
   <React.StrictMode>
