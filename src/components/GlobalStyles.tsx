ae50f988c97842f2ae83ba22e02cf0f342253649
Sergey Zabelin <ns6630@gmail.com>
2021-04-19 02:56:11 +0300

    Добавлены стили для анимаций.

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -3,6 +3,60 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
   html {
     scroll-behavior: smooth;
+
+    @keyframes slideToRight {
+      from {
+        margin-left: -100%;
+        opacity: 0;
+      }
+
+      to {
+        margin-left: 0%;
+        opacity: 1;
+      }
+    }
+
+    @keyframes slideToLeft {
+      from {
+        margin-left: 100%;
+        opacity: 0;
+      }
+
+      to {
+        margin-left: 0%;
+        opacity: 1;
+      }
+    }
+
+    @keyframes compositionIn {
+      from {
+        top: -100%;
+        opacity: 0;
+      }
+
+      to {
+        top: 0;
+        opacity: 1;
+      }
+    }
+  }
+
+  .composition-in {
+    animation-duration: .75s;
+    animation-name: compositionIn;
+    animation-direction: alternate;
+  }
+
+  .slide-to-right {
+    animation-duration: .75s;
+    animation-name: slideToRight;
+    animation-direction: alternate;
+  }
+
+  .slide-to-left {
+    animation-duration: .75s;
+    animation-name: slideToleft;
+    animation-direction: alternate;
   }
 `;
 
